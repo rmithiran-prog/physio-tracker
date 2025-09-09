e807a900a838b3c4e001d32f77aabf6e2296c574
rmithiran-prog <rmithiran@gmail.com>
2025-09-09 18:00:54 +0800

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.Ram.physiotracker',
-  appName: 'Sab physio',
-  webDir: 'build'
-};
-
-export default config;
